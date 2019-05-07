c178a707a670435ad1005c3d8f51827a77d7d4ed
Przemysław Ciąćka <przemyslaw.ciacka@gmail.com>
2019-05-07 22:43:55 +0200

    Remove obsolete code.

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
   methods: {
     ...mapActions(["updateSatellitesData"]),
 
-    async initialize() {
+    initialize() {
       if (this.isAnyVisible) {
         this.turnUpdatesOn();
       }
